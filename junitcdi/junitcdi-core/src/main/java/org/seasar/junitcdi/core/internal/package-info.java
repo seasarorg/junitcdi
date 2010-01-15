dcf7fe5d9acc2c912c40a16a82dafb4cba538fed
Koichi Kobayashi <koichik@improvement.jp>
2010-01-15 18:32:15 +0000

    初期インポート．
    git-svn-id: https://www.seasar.org/svn/sandbox/junitcdi/trunk@3 954ad83d-7a22-4e0d-9814-98b83b46cd04

---
+++ junitcdi/junitcdi-core/src/main/java/org/seasar/junitcdi/core/internal/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * JUnit4のテストクラスでJSR-299 CDI(Contexts and Dependency Injection)を
+ * 利用するための実装クラスを提供するパッケージです．
+ */
+package org.seasar.junitcdi.core.internal;
+
---
+++ junitcdi/junitcdi-core/src/test/java/org/seasar/junitcdi/core/runner/TestClassScopedBean.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2004-2010 the Seasar Foundation and the Others.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, 
+ * either express or implied. See the License for the specific language
+ * governing permissions and limitations under the License.
+ */
+package org.seasar.junitcdi.core.runner;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.enterprise.event.Observes;
+
+import org.junit.Ignore;
+import org.junit.runner.Description;
+import org.seasar.junitcdi.core.TestClassScoped;
+import org.seasar.junitcdi.core.event.TestFinished;
+import org.seasar.junitcdi.core.event.TestStarted;
+
+/**
+ * {@link TestClassScoped}のbean．
+ * 
+ * @author koichik
+ */
+@TestClassScoped
+// Mavenだとテストクラスとして扱われてしまうので．．．
+@Ignore
+public class TestClassScopedBean {
+    /** */
+    public TestClassScopedBean() {
+        System.out.println(toString() + " instanciating.");
+    }
+
+    /** */
+    @PostConstruct
+    public void postConstruct() {
+        System.out.println(toString() + " constructed.");
+    }
+
+    /** */
+    @PreDestroy
+    public void preDestroy() {
+        System.out.println(toString() + " destructing.");
+    }
+
+    /**
+     * @param description
+     */
+    public void onStarted(@Observes @TestStarted final Description description) {
+        System.out.println(toString() + " test started. "
+            + description.getDisplayName());
+    }
+
+    /**
+     * @param description
+     */
+    public void onFinished(@Observes @TestFinished final Description description) {
+        System.out.println(toString() + " test finished. "
+            + description.getDisplayName());
+    }
+}
